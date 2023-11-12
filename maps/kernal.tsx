c67f9b29a376ad0ab4f615d6d8df5eed69c6391a
Lefton <evangowe@gmail.com>
2023-11-12 05:51:28 -0500

    Map Setup-Map 1-3 added

---
+++ maps/kernal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="kernal" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="../../../../Users/Evan/Desktop/LibreSprite/Game Jam#1/kernal.png" width="96" height="32"/>
+</tileset>
