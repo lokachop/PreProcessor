775ca763b30a3364afce9adb0556a94a866a8b82
Lefton <evangowe@gmail.com>
2023-11-12 08:21:01 -0500

    Map and tilessets

---
+++ maps/pipe.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="pipe" tilewidth="32" tileheight="32" tilecount="6" columns="2">
+ <image source="../../../../Users/Evan/Desktop/LibreSprite/Game Jam#1/pipe.png" width="64" height="96"/>
+</tileset>
